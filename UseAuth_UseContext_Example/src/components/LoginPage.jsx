88ed19227dbac72721e7c4e1c2ce1d7ae495b5d5
Seu Nome <seuemail@exemplo.com>
2024-10-30 19:08:19 -0300

    criação do form

--- UseAuth_UseContext_Example/src/components/LoginPage.jsx
+++ UseAuth_UseContext_Example/src/components/LoginPage.jsx
@@ -2,24 +2,45 @@
 import React from 'react';
 import { useAuth } from './AuthProvider';
 import { useNavigate } from 'react-router-dom';
+import '../App.css'
 
 function LoginPage() {
-  const auth = useAuth();
-  const navigate = useNavigate();
+    const auth = useAuth();
+    const navigate = useNavigate();
 
 
-  const handleLogin = () => {
-    auth.login();
-    console.log('Login button clicked');
-    navigate('/');
-  };
+    const handleLogin = (event) => {
+        event.preventDefault()
+        auth.login();
+        navigate('/');
+    };
 
-  return (
-    <div>
-      <h2>Login</h2>
-      <button onClick={handleLogin}>Entrar</button>
-    </div>
-  );
+    return (
+        <>
+        
+           <div className='content_LoginPage'>
+            <h2>Login</h2>
+            
+            <form className='form-box' onSubmit={handleLogin}>
+                <div>
+                    <div className='box_input'>
+                        <p>Email:</p>
+                        <input type="text" />
+                    </div>
+                    <div className='box_input'>
+                        <p>Senha:</p>
+                        <input type="password" />
+                    </div>
+                </div>
+
+                <div>
+                    <button>Entrar</button>
+                </div>
+            </form>
+        </div>
+        </>
+     
+    );
 }
 
 export default LoginPage;
