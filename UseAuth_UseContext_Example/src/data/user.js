bfbd16d7c0b5f50805b7724f1fa291121996ad88
Seu Nome <seuemail@exemplo.com>
2024-10-30 19:08:09 -0300

    criando usuarios ficticios para a autenticação

---
+++ UseAuth_UseContext_Example/src/data/user.js
@@ -0,0 +1,16 @@
+export const user = [
+    {
+        email: 'alexandre020602@gmail.com',
+        password: 'xande123'
+    },
+
+    {
+        email: 'thaunbr28@gmail.com',
+        password: 'thauan123'
+    },
+
+    {
+        email: 'guga13@gmail.com',
+        password: 'malhado13'
+    },
+]
