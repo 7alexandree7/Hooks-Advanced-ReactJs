733cff4f11cc4ec650fd83a24321377f6af7e54a
Seu Nome <seuemail@exemplo.com>
2024-11-02 23:04:42 -0300

    primeira parte

--- useContext_Front-end_Beginners/src/App.jsx
+++ useContext_Front-end_Beginners/src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
+import { Box, Button, Typography } from '@mui/material'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('Front-end Beginners')
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box>
+        <Typography variant='h3' sx={{margin: '20px'}}>
+          {name}
+        </Typography>
+        <ChildrenOne  setName={setName}/>
+      </Box>
     </>
+  )
+}
+
+
+function ChildrenOne({setName}) {
+  return <ChildrenTwo  setName={setName}/>
+}
+
+
+function ChildrenTwo({setName}) {
+
+  return (
+    <Box>
+      <Button sx={{ width: '190px' }} variant="contained" onClick={() => setName('UK Developer')}>
+        Mude o Nome
+      </Button>
+    </Box>
   )
 }
 
