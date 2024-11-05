4c5c1840404bdf766f13ecd90589e1b1cedd340e
Seu Nome <seuemail@exemplo.com>
2024-11-05 15:27:41 -0300

    sx

--- useId/src/Components/FirstExample.jsx
+++ useId/src/Components/FirstExample.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useId } from 'react'
+
+function passwordField () {
+    const passwordHintId = useId()
+}
 
 export default function FirstExample() {
   return (
     <div>
-      
+        <input type="text" aria-describedby={passwordHintId}/>
+        <p id={passwordHintId}></p>
     </div>
   )
 }
